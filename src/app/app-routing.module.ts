c09e459c42684331e237bc1e2cce5e692a13336d
Maciej Kawka <maciej.kawka@objective-partner.com>
2019-06-08 15:54:33 +0200

    added routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CounterComponent } from './counter/counter.component';
+
+const routes: Routes = [{ path: '**', component: CounterComponent }];
+
+@NgModule({ imports: [RouterModule.forRoot(routes)], exports: [RouterModule] })
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppMaterialModuleModule } from './app-material-module/app-material-module.module';
-import { CounterComponent } from './app/counter/counter.component';
+import { CounterComponent } from './counter/counter.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent, CounterComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, AppMaterialModuleModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppMaterialModuleModule,
+    AppRoutingModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
